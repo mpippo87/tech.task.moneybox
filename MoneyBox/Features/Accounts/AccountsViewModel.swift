a31b29c3b93e20dc7e7f5e1c4b018046fe96586f
Filippo Minelle <minelle.filippo@gmail.com>
2023-09-24 17:57:08 +0100

    Added Accounts feature

---
+++ MoneyBox/Features/Accounts/AccountsViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  AccountsViewModel.swift
+//  MoneyBox
+//
+//  Created by Filippo Minelle on 21/09/2023.
+//
+
+import Foundation
+
+final class AccountsViewModel {
+
+    // MARK: - Parameters
+
+    // MARK: - Init
+
+    // MARK: - Methods
+
+}
