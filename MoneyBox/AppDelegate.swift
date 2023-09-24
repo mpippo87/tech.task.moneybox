9e183450e1f68a6c48e944bdff103cf830f87b6a
Filippo Minelle <minelle.filippo@gmail.com>
2023-09-24 17:48:37 +0100

    Lint

--- MoneyBox/AppDelegate.swift
+++ MoneyBox/AppDelegate.swift
@@ -9,9 +9,14 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-    var window: UIWindow? 
+
+    // MARK: - Properties
+
+    var window: UIWindow?
+
+    // MARK: - UIApplicationDelegate
+
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        return true
+        true
     }
 }
-
