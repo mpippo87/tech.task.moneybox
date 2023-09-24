c0ce1db95fd81b28c76dae653301ac252e02087f
Filippo Minelle <minelle.filippo@gmail.com>
2023-09-24 17:56:09 +0100

    Added UI Button and TextFields components

---
+++ Packages/MBUI/Sources/MBUI/Design System/Components/TextField.swift
@@ -0,0 +1,61 @@
+import UIKit
+
+public final class TextField: UITextField {
+
+    // MARK: - Types
+
+    public enum Style {
+        case unknown
+        case email
+        case password
+    }
+
+    // MARK: - Init
+
+    public init(
+        placeholder: String = "",
+        style: Style = .unknown
+    ) {
+        super.init(frame: .zero)
+        self.placeholder = placeholder
+        configure(style: style)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+
+    // MARK: - Methods
+
+    private func setupUI() {
+        borderStyle = .roundedRect
+        layer.cornerRadius = 8
+        layer.borderColor = UIColor.color1.cgColor
+        layer.borderWidth = 1.0
+        heightAnchor.constraint(equalToConstant: 45).isActive = true
+        layer.masksToBounds = true
+
+        attributedPlaceholder = NSAttributedString(
+            string: placeholder ?? "",
+            attributes: [NSAttributedString.Key.foregroundColor: UIColor.color5]
+        )
+        textColor = .color6
+        backgroundColor = .color2
+
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    private func configure(style: Style = .unknown) {
+        switch style {
+        case .email:
+            keyboardType = .emailAddress
+            autocapitalizationType = .none
+            autocorrectionType = .no
+
+        case .password:
+            isSecureTextEntry = true
+
+        case .unknown: break
+        }
+    }
+}
